5cf65b0863738797b190d3a12ca0b111cc0fdb19
julfikar <julfikar6262@gmail.com>
2020-03-06 16:24:12 +0600

    initial add

---
+++ src/assets/AllFriends.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="all-friend">
+        <h2>All friends</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Allfriend',
+        data(){
+            return{
+                friends:[
+                    { name: 'julfikar', online:true},
+                    { name: 'tajal', online:false},
+                    { name: 'siyam', online:true},
+                    { name: 'maruf', online:true},
+                    { name: 'farhana', online:false},
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+           aria-haspopup="true" aria-expanded="false" @click="isOpen = !isOpen">{{ name }} <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu" >
+
+            <li v-for="(item, index) in items" :key="index"><a :href="item.link">{{ item.title }}</a></li>
+
+        </ul>
+    </li>
+</template>
+
+<script>
+
+    export default {
+        name: "Dropdown",
+        props: ['name', 'items'],
+        data() {
+            return {
+                isOpen: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
